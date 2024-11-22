74318dc9c7b9c9e23ec0d550e412aa76f3b856a2
Martijn Kunstman <martijn@kunstman.nl>
2024-11-22 11:00:34 +0100

    init

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ include("reactions.php");
 
 $getReactions = Reactions::getReactions();
 //uncomment de volgende regel om te kijken hoe de array van je reactions eruit ziet
-// echo "<pre>".var_dump($getReactions)."</pre>";
+
 
 if(!empty($_POST)){
 
     //dit is een voorbeeld array.  Deze waardes moeten erin staan.
     $postArray = [
-        'name' => "Ieniminie",
+        'name' => $_POST['naam'],
         'email' => "ieniminie@sesamstraat.nl",
-        'message' => "Geweldig dit"
+        'message' => $_POST['message']
     ];
 
     $setReaction = Reactions::setReaction($postArray);
@@ -35,8 +35,26 @@ if(!empty($_POST)){
 <body>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=twI61ZGDECBr4ums" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-    <h2>Hieronder komen reacties</h2>
+   
+   <h2>hier komt het formulier om een bericht te versturen</h2>
+   <form action="" method="post">
+        <input type="text" placeholder="..vul hier je naam in" name="naam">
+        <br>
+        <textarea id="w3review"  placeholder="..vul hier je bericht in" name="message"  rows="4" cols="50"></textarea>
+        <br>
+        <input type="submit" value="versturen">
+
+    </form>
+   
+        <h2>Hieronder komen reacties</h2>
     <p>Maak hier je eigen pagina van aan de hand van de opdracht</p>
+
+<?php
+echo "<pre>".var_dump($getReactions)."</pre>";
+echo ("<h2>".count($getReactions)."</h2>");
+?>
+
+
 </body>
 </html>
 
